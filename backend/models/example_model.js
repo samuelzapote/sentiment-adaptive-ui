06f09b4571b2bb9e1647887a45306b41a7ad59ce
Xavier Brown <secton67@gmail.com>
2019-02-23 19:21:15 -0600

    purge backend

--- backend/models/example_model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const exampleSchema = mongoose.Schema({
-    name: { type: String },
-    body: { type: String }
-});
-
-module.exports = mongoose.model('exampleModel', exampleSchema);
